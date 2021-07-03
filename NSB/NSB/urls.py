eabbe842a42bb3fd19fb2709e8bd3b3c0ea00dcc
dragon0821 <dydrkfsla98@naver.com>
2021-07-03 13:22:54 +0900

    service_evalu_create check please

---
+++ NSB/NSB/urls.py
@@ -0,0 +1,47 @@
+"""NSB URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+#id이런 값을 html에서 받을때 받아야함 그래야 view에 있는 함수들을 실행할 수 있다
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home, name="home"),
+    path('login/',views.login,name="login"),
+
+    path('user/<int:id>',views.user_read,name="user_read"),
+    path('user/new',views.user_create,name="user_create"),
+
+    path('service/new', views.service_create, name="service_create"),
+    path('service/<int:id>', views.service_read, name="service_read"),
+    
+    path('idea/new', views.idea_create, name="idea_create"),
+    path('idea/<int:i_id>', views.idea_read, name="idea_read"),
+    path('idea/comment_create/<int:i_id>', views.idea_comment_create, name="idea_comment_create"),
+    path('idea/update/<int:i_id>', views.idea_update, name="idea_update"),
+    path('idea/delete/<int:i_id>', views.idea_delete, name="idea_delete"),
+    path('idea/comment_update/<int:idea_upload_id>/<int:c_id>', views.idea_comment_update, name="idea_comment_update"),
+    path('idea/comment_delete/<int:idea_upload_id>/<int:c_id>', views.idea_comment_delete, name="idea_comment_delete"),
+  
+    path('service/<int:s_id>/evalu/<int:e_id>', views.s_evalu_read, name="s_evalu_read"),
+    path('service/<int:s_id>/evalu/new', views.s_evalu_create, name="s_evalu_create"), # id가 service의 값으로 들어오는 것 같음
+
+    path('service/<str:service_upload_id>/evalu/<int:e_id>/comment',views.s_evalu_comment_create,name="s_evalu_comment_create"),
+
+    path('mypage/',views.mypage_read, name="mypage"),
+]
